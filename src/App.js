210a656626e4ef545e29118280168b5686bcf0d3
John Lockhart <lockhartjohn527@gmail.com>
2021-12-30 15:35:30 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState (
+    [
+        {
+            id: 1,
+            text: 'USPO appt',
+            day: 'Jan 1st at 2:00pm',
+            reminder: false,
+        },
+        {
+            id: 2,
+            text: 'Drs appt',
+            day: 'Jan 1st at 3:00pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Dinner w/ boo',
+            day: 'Feb 1st at 9:00pm',
+            reminder: true,
+        }
+        ]
+)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Header />
+    <Tasks tasks={tasks}/>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,14 @@
+import Task from "./Task"
+
+const Tasks = ({tasks}) => {
+    
+    return (
+        <>
+          {tasks.map((task) => (
+          <h3 key={task.id}> {task.text} </h3>))} 
+
+        </>
+    )
+}
+
+export default Tasks
